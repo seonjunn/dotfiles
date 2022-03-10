99a1075c2cdf1be2f21bdf325e7e0d864a190800
Seonjun Kim <cyanide17@snu.ac.kr>
2022-03-10 16:41:50 +0900

    abbr

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,7 @@ end
 # Mute fish greeting
 set fish_greeting
 
-# Language as engligh
+# lang as eng
 export LC_ALL=C
 
 # PATHS
@@ -16,13 +16,27 @@ export PATH="$HOME/Library/Python/3.8/bin:$PATH"
 # Plugins
 zoxide init fish | source
 
-# aliases
-alias cat   "bat"
-alias l    "exa -l"
-alias rm    "rm -i"
-alias v     "vim"
-alias py    "python3"
+# abbr
+abbr -a cat "bat"
+abbr -a v   "vim"
+abbr py     "python3"
 
+if command -v exa > /dev/null
+	abbr -a l   'exa'
+	abbr -a ls  'exa'
+	abbr -a ll  'exa -l'
+	abbr -a lll 'exa -la'
+else
+	abbr -a l   'ls'
+	abbr -a ll  'ls -l'
+	abbr -a lll 'ls -la'
+end
+
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate ''
+set __fish_git_prompt_showupstream 'none'
+set -g fish_prompt_pwd_dir_length 3
 
 function fish_prompt
 	set_color brblack
